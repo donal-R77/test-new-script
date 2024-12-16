c9ebfe7c0636dfb9ae4edada96a4d041afc184e0
donal-R77 <rakotoarisoaromarick@gmail.com>
2024-12-16 18:42:15 +0300

    fix: test a new way again

--- initial_script.sh
+++ initial_script.sh
@@ -18,6 +18,4 @@ sudo chown root:docker /var/run/docker.sock
 sudo chmod a+w /var/run/docker.sock
 gcloud storage cp gs://deploy-file/frontend-script/bash_test.sh .
 chmod a+x *.sh
- DOCKER_HUB_USERNAME=$DOCKER_HUB_USERNAME
- DOCKER_HUB_TOKEN=$DOCKER_HUB_TOKEN
- . bash_test.sh
+#add
