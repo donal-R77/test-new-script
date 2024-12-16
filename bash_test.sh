ffdd62ce832ba11b9af9ec06a12c9ebfa670b1b8
donal-R77 <rakotoarisoaromarick@gmail.com>
2024-12-16 17:05:14 +0300

    add export var

--- bash_test.sh
+++ bash_test.sh
@@ -2,7 +2,8 @@
 
 PORTS="5000:5000"
 CONTAINER_NAME="flex"
-IMAGE_NAME="refonte/flexbox:latest"
+REPOSITORY="refonte/refonte-images"
+IMAGE_NAME="latest"
 
 #login to Docker Hub
 echo "${DOCKER_HUB_TOKEN}" | docker login -u "${DOCKER_HUB_USERNAME}" --password-stdin
@@ -14,7 +15,7 @@ docker pull "${IMAGE_NAME}"
 if [[ $(docker ps -qqa -f name="${CONTAINER_NAME}") ]] ; then 
   docker container stop flex && docker container rm flex
 fi 
-docker run -d -p ${PORTS} --name ${CONTAINER_NAME} ${IMAGE_NAME}
+docker run -d -p ${PORTS} --name ${CONTAINER_NAME} ${REPOSITORY}:${latest}
 
 #remove
 docker container prune -f 
