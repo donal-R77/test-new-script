971374e5126e2047db8f6ab9fe4e048c5fda287e
donal-R77 <rakotoarisoaromarick@gmail.com>
2024-12-16 15:25:44 +0300

    script modified

--- initial_script.sh
+++ initial_script.sh
@@ -4,7 +4,7 @@ sudo systemctl start docker
 sudo systemctl enable docker
 sudo chown root:docker /var/run/docker.sock
 sudo chmod a+w /var/run/docker.sock
-gcloud storage cp gs://deploy-file/frontend-script/bash_test.sh
+gcloud storage cp gs://deploy-file/frontend-script/bash_test.sh .
 chmod a+x *.sh
 DOCKER_HUB_USERNAME=$DOCKER_HUB_USERNAME
 DOCKER_HUB_TOKEN=$DOCKER_HUB_TOKEN
