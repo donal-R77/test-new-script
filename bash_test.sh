44ffe43246d10513455d4f53f3c7392ab14d38dd
donal-R77 <rakotoarisoaromarick@gmail.com>
2024-12-16 16:04:58 +0300

    test something

--- bash_test.sh
+++ bash_test.sh
@@ -2,10 +2,10 @@
 
 PORTS="5000:5000"
 CONTAINER_NAME="flex"
-IMAGE_NAME="maric77/flexbox:flex"
+IMAGE_NAME="maric77/flexbox:latest"
 
 #login to Docker Hub
-echo "${DOCKER_HUB_TOKEN}" | docker login -u "${DOCKER_HUB_USERNAME}" --password-stdin
+#echo "${DOCKER_HUB_TOKEN}" | docker login -u "${DOCKER_HUB_USERNAME}" --password-stdin
 
 #Pull image in DockerHub
 docker pull "${IMAGE_NAME}"
